b733e3b62a66745f8c1e49a65fdbc19323d772d4
Trejon McGee <trejon.mcgee@gmail.com>
2023-11-15 23:13:15 -0600

    Added new codewars challenge

---
+++ 8kyu/Square(n) Sum.py
@@ -0,0 +1,11 @@
+# DESCRIPTION:
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+# My Solution
+
+def square_sum(numbers):
+    sum = 0
+    for num in numbers:
+        num = num ** 2
+        sum += num
+    return sum
