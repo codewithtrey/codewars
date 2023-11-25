cf815ce2f3a79e9aa4d02f6c7cc11b721a9944a5
Trejon McGee <trejon.mcgee@hotmail.com>
2023-11-24 23:44:12 -0600

    Added a new codewars challenge

---
+++ 8kyu/Convert a Number to a String!.py
@@ -0,0 +1,15 @@
+# Description
+
+# We need a function that can transform a number (integer) into a string.
+
+# What ways of achieving this do you know?
+
+
+# My Solution
+def number_to_string(num):
+    return str(num)
+
+
+# Uncomment to run and check test cases
+# num = 300
+# print(number_to_string(num))
