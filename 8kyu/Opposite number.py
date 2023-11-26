83d7e023a38bdfd3ad90934ff6b6a12e6e26719c
Trejon McGee <trejon.mcgee@gmail.com>
2023-11-26 15:06:26 -0600

    Added a new codewars challenge

---
+++ 8kyu/Opposite number.py
@@ -0,0 +1,19 @@
+# Description
+
+# Very simple, given an integer or a floating-point number, find its opposite.
+
+# Examples:
+
+# 1: -1
+# 14: -14
+# -34: 34
+
+
+# My Solution
+def opposite(number):
+    return -number
+
+
+# Uncomment to run and check test cases
+# number = 5
+# print(opposite(number))
