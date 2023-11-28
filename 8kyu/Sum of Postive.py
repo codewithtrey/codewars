0d46702388fb37435eb0a09e564ce62636a26db9
Trejon McGee <trejon.mcgee@gmail.com>
2023-11-27 23:15:09 -0600

    Added a new codewars challenge

---
+++ 8kyu/Sum of Postive.py
@@ -0,0 +1,18 @@
+# Description
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+
+
+# My Solution
+def positive_sum(arr):
+    positive_numbers = [num for num in arr if num > 0]
+
+    return sum(positive_numbers)
+
+
+# Uncomment to run and check test cases
+# arr = [1, -4, 7, 12]
+# print(positive_sum(arr))
