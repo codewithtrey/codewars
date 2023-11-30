a38e6e3fa15814a6362a8f7d92b262d2d540483c
Trejon McGee <trejon.mcgee@gmail.com>
2023-11-29 23:01:39 -0600

    Added a new codewars challenge

---
+++ 8kyu/Reversed Strings.py
@@ -0,0 +1,15 @@
+# Description
+
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+
+# My Solution
+def reverse_string(string):
+    return string[::-1]
+
+# Uncomment to run and check test cases
+# result1 = reverse_string('world')
+# print(result1)
