68582346ac7c653da4a62fd7cf7fd1042bde6093
Trejon McGee <trejon.mcgee@gmail.com>
2024-01-06 20:42:45 -0600

    Added a new codewars challenge

---
+++ 8kyu/Basic Mathematical Operations.py
@@ -0,0 +1,31 @@
+# Description
+
+# Your task is to create a function that does four basic mathematical operations.
+
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+
+# Examples(Operator, value1, value2) --> output
+# ('+', 4, 7) --> 11
+# ('-', 15, 18) --> -3
+# ('*', 5, 5) --> 25
+# ('/', 49, 7) --> 7
+
+# My Solution
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        return value1 + value2
+    elif operator == '-':
+        return value1 - value2
+    elif operator == '*':
+        return value1 * value2
+    elif operator == '/':
+        return value1 / value2
+    else:
+        raise ValueError("Invalid operator")
+
+
+# Uncomment to run and check test case
+# result1 = basic_op('+', 4, 7)
+# ('+', 4, 7) --> 11
+# print(result1)
