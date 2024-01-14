ebab2959b7511558c9148d042cc3c2eb1b04abae
Trejon McGee <trejon.mcgee@hotmail.com>
2024-01-14 14:13:36 -0600

    Added a new codewars challenge

---
+++ 8kyu/Function 1 - hello world.py
@@ -0,0 +1,12 @@
+# Description
+
+# Make a simple function called greet that returns the most-famous "hello world!".
+
+
+# My Solution
+def greet():
+    return "hello world!"
+
+
+# Uncomment to run and check test case
+# print(greet())  # Output: "hello world!"
