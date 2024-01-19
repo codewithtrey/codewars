f2087c9d6abb68d803b7287d2a42f3ad2be07680
Trejon McGee <trejon.mcgee@hotmail.com>
2024-01-18 21:03:00 -0600

    Added a new codewars challenge

---
+++ 8kyu/Beginner Series #1 School Paperwork.py
@@ -0,0 +1,22 @@
+# Description
+
+# Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+# Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+# Example:
+# n= 5, m=5: 25
+# n=-5, m=5:  0
+
+
+# My Solution
+def paperwork(n, m):
+    if n < 0 or m < 0:
+        return 0
+    else:
+        return n * m
+
+
+# Uncomment to run and check test case
+# print(paperwork(5, 5))  # Output: 25 (5 classmates *  5 pages per classmate)
+# print(paperwork(-5, 5))  # Output: 0 (Negative n, should return 0)
