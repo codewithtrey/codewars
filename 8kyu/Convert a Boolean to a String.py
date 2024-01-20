a83d1ed6cddcab07e5f68a5f773e225935a546a7
Trejon McGee <trejon.mcgee@hotmail.com>
2024-01-19 20:34:48 -0600

    Added a new codewars challenge

---
+++ 8kyu/Convert a Boolean to a String.py
@@ -0,0 +1,15 @@
+# Description
+
+# Implement a function which convert the given boolean value into its string representation.
+
+# Note: Only valid inputs will be given.
+
+
+# My Solution
+def boolean_to_string(b):
+    return str(b)
+
+
+# Uncomment to run and check test case
+# print(boolean_to_string(True))  # Output: "True"
+# print(boolean_to_string(False)) # Output: "False"
