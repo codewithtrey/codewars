640d584b81cfdbaac900b1e86f5e2b4d28a68601
Trejon McGee <trejon.mcgee@hotmail.com>
2024-01-31 11:03:32 -0600

    Added a new codewars challenge

---
+++ 8kyu/MakeUpperCase.py
@@ -0,0 +1,14 @@
+# Description
+
+# Write a function which converts the input string to uppercase.
+
+
+# My Solution
+def make_upper_case(s):
+    return s.upper()
+
+
+# Uncomment to run and check test case
+# input_string = "Hello, World!"
+# result = make_upper_case(input_string)
+# print(result)  # Output: "HELLO, WORLD!"
