47d3242f863d339dcad61b47c4bfec1fb92ba241
Trejon McGee <trejon.mcgee@hotmail.com>
2024-01-30 18:51:12 -0600

    Added a new codewars challenge

---
+++ 8kyu/Beginner - Reduce but Grow.py
@@ -0,0 +1,17 @@
+# Description
+
+# Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+# My Solution
+def grow(arr):
+    result = 1
+    for num in arr:
+        result *= num
+    return result
+
+
+# Uncomment to run and check test case
+print(grow([1, 2, 3, 4]))  # Output: 24
