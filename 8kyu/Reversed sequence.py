e83d7d3057af474af323f3021514d1fd73914ab0
Trejon McGee <trejon.mcgee@hotmail.com>
2024-02-01 08:25:49 -0600

    Added a new codewars challenge

---
+++ 8kyu/Reversed sequence.py
@@ -0,0 +1,14 @@
+# Description
+
+# Build a function that returns an array of integers from n to 1 where n>0.
+
+# Example : n=5 --> [5,4,3,2,1]
+
+
+# My Solution
+def reverse_seq(n):
+    return list(range(n, 0, -1))
+
+
+# Uncomment to run and check test case
+# print(reverse_seq(5))  # Output: [5, 4, 3, 2, 1]
