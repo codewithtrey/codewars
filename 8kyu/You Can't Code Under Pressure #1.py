e50e2000220389f49e33977618f5cbd5a11cfb00
Trejon McGee <trejon.mcgee@hotmail.com>
2024-02-07 15:34:00 -0600

    Added a new codewars challenge

---
+++ 8kyu/You Can't Code Under Pressure #1.py
@@ -0,0 +1,13 @@
+# Description
+
+# Code as fast as you can! You need to double the integer and return it.
+
+
+# My Solution
+def double_integer(i):
+    return i * 2
+
+
+# Uncomment to run and check test cases
+print(double_integer(2))  # Output: 4
+print(double_integer(4))  # Output: 8
