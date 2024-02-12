f60f45565f24c876d41ba0c5de1394d590b8412a
Trejon McGee <trejon.mcgee@hotmail.com>
2024-02-11 22:38:37 -0600

    Added a new codewars challenge

---
+++ 8kyu/Find Maximum and Minimum Values of a List.py
@@ -0,0 +1,34 @@
+# Description
+
+# Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+# Examples (Input -> Output)
+# * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+# * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+# * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+# * [5]                             -> min = 5, max = 5
+# Notes
+# You may consider that there will not be any empty arrays/vectors.
+
+
+# My Solution
+def minimum(arr):
+    return min(arr)
+
+
+def maximum(arr):
+    return max(arr)
+
+
+# Uncomment to run and check test cases
+arr1 = [4, 6, 2, 1, 9, 63, -134, 566]
+print("min =", minimum(arr1), ", max =", maximum(arr1))
+
+arr2 = [-52, 56, 30, 29, -54, 0, -110]
+print("min =", minimum(arr2), ", max =", maximum(arr2))
+
+arr3 = [42, 54, 65, 87, 0]
+print("min =", minimum(arr3), ", max =", maximum(arr3))
+
+arr4 = [5]
+print("min =", minimum(arr4), ", max =", maximum(arr4))
