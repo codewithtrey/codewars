f2e030d855c92d5d30b45d133ef2b307af9c3fd0
Trejon McGee <trejon.mcgee@hotmail.com>
2024-04-03 17:17:08 -0400

    Added a new codewars challenge

---
+++ 8kyu/Volume of a Cuboid.py
@@ -0,0 +1,13 @@
+# Description
+
+# Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+
+# My Solution
+def get_volume_of_cuboid(length, width, height):
+    return length * width * height
+
+
+# Uncomment to run and check test cases
+# print(get_volume_of_cuboid(1,2,2))  # Output: 4
+# print(get_volume_of_cuboid(6.3, 2, 5))  # Output: 63
