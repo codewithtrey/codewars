ae85df7a58804c5afb57fd8554126acb242d74d7
Trejon McGee <trejon.mcgee@hotmail.com>
2024-04-11 20:11:26 -0400

    Added a new codewars challenge

---
+++ 8kyu/Cat years, Dog years.py
@@ -0,0 +1,44 @@
+# Description
+
+# I have a cat and a dog.
+
+# I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+# Return their respective ages now as [humanYears,catYears,dogYears]
+
+# NOTES:
+
+# humanYears >= 1
+# humanYears are whole numbers only
+# Cat Years
+# 15 cat years for first year
+# +9 cat years for second year
+# +4 cat years for each year after that
+# Dog Years
+# 15 dog years for first year
+# +9 dog years for second year
+# +5 dog years for each year after that
+
+
+# My Solution
+def human_years_cat_years_dog_years(human_years):
+    cat_years = 0
+    dog_years = 0
+
+    if human_years == 1:
+        cat_years = 15
+        dog_years = 15
+    elif human_years == 2:
+        cat_years = 24
+        dog_years = 24
+    else:
+        cat_years = 24 + (human_years - 2) * 4
+        dog_years = 24 + (human_years - 2) * 5
+
+    return [human_years, cat_years, dog_years]
+
+
+# Uncomment to run and check test cases
+# print(human_years_cat_years_dog_years(1))  # Output: [1, 15, 15]
+# print(human_years_cat_years_dog_years(2))  # Output: [2, 24, 24]
+# print(human_years_cat_years_dog_years(5))  # Output: [5, 36, 39]
